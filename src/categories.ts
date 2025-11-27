4f9938a3ec44ae9383c62aeec1931aad54bcb174
arturogutierrez00 <arturo.g@bidcom.com.ar>
2025-11-27 14:24:44 -0300

    Feature/int-fix&retry

--- src/categories.ts
+++ src/categories.ts
@@ -3,6 +3,7 @@ import { CategoriesModule } from './app/module/Categories.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(CategoriesModule);
+  app.enableCors();
 
   await app.listen(3300, '0.0.0.0'); // puerto dedicado para este servicio
 
